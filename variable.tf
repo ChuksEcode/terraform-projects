e4e18508385b0d9ee25d55f1995344585e66cdd8
Chuks Emenike <chukschuksemenike@gmail.com>
2023-07-03 22:33:50 +0200

    created route53 file

--- variable.tf
+++ variable.tf
@@ -108,6 +108,19 @@ variable "ec2_instance_type" {
 
 variable "ec2_key_pair_name" {
   default       = "name of keypair"
-  description   = " name of the ec2 keypair"
+  description   = "name of the ec2 keypair"
+  type          = string
+}
+
+# route 53 variables
+variable "domain_name" {
+  default       = "chuksemenike.com"
+  description   = "domain name"
+  type          = string
+}
+
+variable "record_name" {
+  default       = "www"
+  description   = "sub domain name"
   type          = string
 }
